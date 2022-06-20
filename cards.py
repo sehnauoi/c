dea99a5f0e4753c3ce17a7dbf7fc1c430564640f
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-21 01:19:23 +0900

    Update cards.py

--- cards.py
+++ cards.py
@@ -63,13 +63,13 @@ def extract(): #Get Hash and files name (also raw)
   # downloading 3dUnity
   for x,y in zip(file,hash):
     link = "http://prd-priconne-redive.akamaized.net/dl/pool/AssetBundles/"+y[0:2]+"/"+y
-    path = r'raw/'+x
+    path = r'./public/images/raw/'+x
     urllib.request.urlretrieve(link, path)
     print("Downloading "+link+"\n"+y+"\nin "+path+"\nas "+x+"\n==========================")
     
-  w = r'/cards/'+x[0:21]+'png'
+  w = r'/cards/'+x[14:21]+'png'
   os.system("deserialize.py ")
-  subprocess.call(['python', 'deserialize.py', x, w])
+  subprocess.call(['python', 'deserialize.py', path, w])
   print("Download Finished!")
 
 # http://prd-priconne-redive.akamaized.net/dl/pool/AssetBundles/${hash.substr(0, 2)}/${hash}
