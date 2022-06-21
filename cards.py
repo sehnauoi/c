bcccffe647bc8700b321337202edf1efcd4fd48b
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-21 20:10:17 +0900

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,19 +1,14 @@
-# import dependencies
-# import pandas as pd
-# from selenium import webdriver
-# from bs4 import BeautifulSoup
-# from tika import parser
-# import re
-# import sys
 import urllib.request
 import os
 import requests
-import subprocess
+import UnityPy
 
 #data lists
 raw = []
 file = []
 hash = []
+id = []
+name = []
 
 def get_files(): # get data from url
 
@@ -44,6 +39,9 @@ def get_all(): # get all values for extracting with regex
               raw.append(line[start:start+47])
               file.append(line[start:start+28])
               hash.append(line[start+29:start+61])
+              id.append(line[start+16:start+22])
+              name.append(line[start+5:start+22])
+
 
 def report(): # reporting
   print("""
@@ -59,25 +57,50 @@ def report(): # reporting
   """)
   print(file[:10])
 
-def extract(): #Get Hash and files name (also raw)
+def download_unity(): #Get Hash and files name (also raw)
   # downloading 3dUnity
   for x,y in zip(file,hash):
     link = "http://prd-priconne-redive.akamaized.net/dl/pool/AssetBundles/"+y[0:2]+"/"+y
-    path = r'./public/images/raw/'+x
+    path = r'./raw/'+x
     urllib.request.urlretrieve(link, path)
-    print("Downloading "+link+"\n"+y+"\nin "+path+"\nas "+x+"\n==========================")
-    
-  print("Download Finished!\n==========================")
+    print("Downloading "+link+"\n"+y+"\nin "+path+"\nas "+x+"\n==========================")  
+  print("Download Finished!")
+
+  # w = r'/cards/'+x[14:21]+'png'
+  # os.system("deserialize.py ")
+  # subprocess.call(['python', 'deserialize.py', x, w])
   
-  print("Extracting...")  # extracting
-  for x in file:
-    w = r'/cards/'+x[14:21]+'png'
-    os.system('./public/images/deserialize.py')
-    subprocess.call(['sudo','python', 'deserialize.py', path, w])
-  
-  print("Extract Finished!!!")
-   
+def unpack_all_assets(source_folder : str, destination_folder : str):
+    # iterate over all files in source folder
+    for root, dirs, files in os.walk(source_folder):
+        for file_name in files:
+            # generate file_path
+            file_path = os.path.join(root, file_name)
+            # load that file via UnityPy.load
+            env = UnityPy.load(file_path)
+
+            # iterate over internal objects
+            for obj in env.objects:
+                # process specific object types
+                if obj.type.name in ["Texture2D"]:
+                    # parse the object data
+                    data = obj.read()
+
+                    # create destination path
+                    dest = os.path.join(destination_folder, data.name[11:17])
+
+                    # make sure that the extension is correct
+                    # you probably only want to do so with images/textures
+                    dest, ext = os.path.splitext(dest)
+                    dest = dest + ".png"
+
+                    img = data.image
+                    img.save(dest)
+                    print("Extracting "+data.name[11:17]+".png"+"\n==========================")
+    print("Extract Completed!")
+
 get_files()
 get_all()
 report()
-extract()
+download_unity()
+unpack_all_assets('./raw/', './cards/')
